d2119fdc12edff6438b4d9ae2aa0322f2b2b5c0a
kkulik112 <kkulik112@gmail.com>
2023-03-04 10:05:44 +0100

    Google map integration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+/// <reference types="@types/google.maps" />
 import {User} from './User'
 import {Company} from './Company'
 
 const user = new User()
 const company = new Company()
+
+const mapContainer = document.getElementById('map') as HTMLElement
+new google.maps.Map(mapContainer, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
 
 console.log(user)
 console.log(company)
